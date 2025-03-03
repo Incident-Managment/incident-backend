3ad93c7ec5642155019a1b07382c4ec54b4422cd
JavierValenzuelaEsparza <javier.valenzuela@g-global.com>
2025-03-02 23:58:42 -0800

    feat(Firebase) improve firebase

--- actions/Incidents/createIncident.js
+++ actions/Incidents/createIncident.js
@@ -28,6 +28,9 @@ module.exports = {
                 const filename = `incidents/${Date.now()}`;
                 this.logger.info("Uploading file to Firebase Storage with filename:", filename);
 
+                // Log before calling the upload service
+                this.logger.info("Calling storageService.uploadImage");
+
                 const uploadResult = await ctx.call("storageService.uploadImage", {
                     file: fileBuffer,
                     filename: filename
