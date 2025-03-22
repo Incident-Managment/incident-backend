32451c7601c718d25c6ab93198ecf89c9c255fb0
JavierValenzuelaEsparza <javier.valenzuela@g-global.com>
2025-03-22 02:23:02 -0700

    feat: Improves in the createIncidents and the notification supervisors

--- actions/Incidents/createIncident.js
+++ actions/Incidents/createIncident.js
@@ -26,7 +26,7 @@ module.exports = {
             try {
                 const fileBuffer = fs.readFileSync(imagePath);
                 const filename = `incidents/${Date.now()}`;
-                
+
                 const uploadResult = await ctx.call("storageService.uploadImage", {
                     file: fileBuffer,
                     filename: filename
@@ -72,24 +72,37 @@ module.exports = {
 
             ctx.emit("incident.created", { incident });
             this.logger.info("Incidencia creada con éxito:", incident);
-            
+
             setImmediate(async () => {
                 try {
                     const users = await ctx.call("users.getUsersGlobal");
+                    const machines = await ctx.call("machines.getMachinesGlobal");
+                    const statuses = await ctx.call("statuses.getStatuses");
+                    const priorities = await ctx.call("priorities.getPrioritiesByCompany", { companyId: company_id });
+                    const categories = await ctx.call("categories.getCategoriesByCompany", { companyId: company_id });
+                    const productionPhases = await ctx.call("production_phases.getProductionPhasesByCompany", { companyId: company_id });
+
                     const filteredUsers = users.filter(user => user.role.id === 2 && user.company.id === company_id);
                     const templatePath = path.join(__dirname, '../../templates/notificationToSupervisor.html');
                     let template = fs.readFileSync(templatePath, 'utf8');
 
+                    const user = users.find(u => u.id === user_id);
+                    const machine = machines.find(m => m.id === machine_id);
+                    const status = statuses.find(s => s.id === status_id);
+                    const priority = priorities.find(p => p.id === priority_id);
+                    const category = categories.find(c => c.id === category_id);
+                    const productionPhase = productionPhases.find(pp => pp.id === production_phase_id);
+
                     template = template.replace('{{title}}', title)
-                                       .replace('{{description}}', description)
-                                       .replace('{{user_id}}', user_id)
-                                       .replace('{{machine_id}}', machine_id)
-                                       .replace('{{category_id}}', category_id)
-                                       .replace('{{production_phase_id}}', production_phase_id)
-                                       .replace('{{priority_id}}', priority_id)
-                                       .replace('{{status_id}}', status_id)
-                                       .replace('{{creation_date}}', creation_date || now)
-                                       .replace('{{update_date}}', update_date || now);
+                        .replace('{{description}}', description)
+                        .replace('{{user_id}}', user ? user.name : user_id)
+                        .replace('{{machine_id}}', machine ? machine.name : machine_id)
+                        .replace('{{category_id}}', category ? category.name : category_id)
+                        .replace('{{production_phase_id}}', productionPhase ? productionPhase.name : production_phase_id)
+                        .replace('{{priority_id}}', priority ? priority.name : priority_id)
+                        .replace('{{status_id}}', status ? status.name : status_id)
+                        .replace('{{creation_date}}', creation_date || now)
+                        .replace('{{update_date}}', update_date || now);
 
                     for (const user of filteredUsers) {
                         const emailParams = {
